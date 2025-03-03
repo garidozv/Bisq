7f6a565d7a5273bfab1482c157bc9382a8665132
garidos <113151588+garidos@users.noreply.github.com>
2025-03-03 23:17:13 +0100

    Create empty class for semantic analyzer and update test class to perform semantic analysis

---
+++ src/rs/ac/bg/etf/pp1/SemanticAnalyzer.java
@@ -0,0 +1,40 @@
+package rs.ac.bg.etf.pp1;
+
+import org.apache.log4j.Logger;
+
+import rs.ac.bg.etf.pp1.ast.SyntaxNode;
+import rs.ac.bg.etf.pp1.ast.VisitorAdaptor;
+import rs.etf.pp1.symboltable.concepts.Obj;
+
+public class SemanticAnalyzer extends VisitorAdaptor {
+	
+	int printCallCount = 0;
+	int varDeclCount = 0;
+	Obj currentMethod = null;
+	boolean returnFound = false;
+	boolean errorDetected = false;
+	int nVars;
+	
+	Logger log = Logger.getLogger(getClass());
+
+	public void report_error(String message, SyntaxNode info) {
+		errorDetected = true;
+		StringBuilder msg = new StringBuilder(message);
+		int line = (info == null) ? 0: info.getLine();
+		if (line != 0)
+			msg.append (" na liniji ").append(line);
+		log.error(msg.toString());
+	}
+
+	public void report_info(String message, SyntaxNode info) {
+		StringBuilder msg = new StringBuilder(message); 
+		int line = (info == null) ? 0: info.getLine();
+		if (line != 0)
+			msg.append (" na liniji ").append(line);
+		log.info(msg.toString());
+	}
+	
+	public boolean passed(){
+    	return !errorDetected;
+    }
+}
---
+++ test/rs/ac/bg/etf/pp1/Compiler.java
@@ -0,0 +1,80 @@
+package rs.ac.bg.etf.pp1;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.xml.DOMConfigurator;
+
+import java_cup.runtime.Symbol;
+import rs.ac.bg.etf.pp1.util.Log4JUtils;
+import rs.etf.pp1.symboltable.Tab;
+import rs.etf.pp1.symboltable.concepts.Obj;
+import rs.etf.pp1.symboltable.concepts.Struct;
+import rs.ac.bg.etf.pp1.ast.Program;
+
+public class Compiler {
+
+	static {
+		DOMConfigurator.configure(Log4JUtils.instance().findLoggerConfigFile());
+		Log4JUtils.instance().prepareLogFile(Logger.getRootLogger());
+	}
+	
+	public static void main(String[] args) throws Exception {
+		
+		Logger log = Logger.getLogger(Compiler.class);
+		
+		Reader br = null;
+		try {
+			File sourceCode = new File("test/program.mj");
+			log.info("Compiling source file: " + sourceCode.getAbsolutePath());
+			
+			// Lexical analysis
+			br = new BufferedReader(new FileReader(sourceCode));
+			Yylex lexer = new Yylex(br);
+			
+			// Syntactic analysis
+			MJParser parser = new MJParser(lexer);
+			//parser.loggingEnabled = true;
+	        Symbol rootNode = parser.parse();  
+
+	        Program programNode = (Program)(rootNode.value); 
+			
+	        /*
+			// Log syntax tree
+			log.info(prog.toString(""));
+			log.info("===================================");
+			*/
+			
+	        // Symbol table initialization
+			Tab.init();
+			
+			Struct boolTypeStruct = new Struct(Struct.Bool);
+			Obj boolTypeObj = Tab.insert(Obj.Type, "bool", boolTypeStruct);
+			boolTypeObj.setAdr(-1);
+			boolTypeObj.setLevel(-1);
+			
+			// Semantic analysis
+			SemanticAnalyzer semAnalyzer = new SemanticAnalyzer();
+			programNode.traverseBottomUp(semAnalyzer);
+			
+			Tab.dump();
+			log.info("===================================");
+			
+			if (!parser.errorDetected && semAnalyzer.passed()) {
+				log.info("Program successfully parsed!");
+			} else {
+				log.info("Program parsing was unsuccessful!");
+			}
+		} 
+		finally {
+			if (br != null) try { br.close(); } catch (IOException e1) { log.error(e1.getMessage(), e1); }
+		}
+
+	}
+	
+	
+}

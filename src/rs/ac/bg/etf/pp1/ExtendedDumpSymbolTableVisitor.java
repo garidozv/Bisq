f9091fb725a61248dd6e9ebadc058e43709bc335
garidos <113151588+garidos@users.noreply.github.com>
2025-03-22 02:15:17 +0100

    Fix dump output for interface arrays and add couple of code generation tests

--- src/rs/ac/bg/etf/pp1/ExtendedDumpSymbolTableVisitor.java
+++ src/rs/ac/bg/etf/pp1/ExtendedDumpSymbolTableVisitor.java
@@ -39,6 +39,9 @@ public class ExtendedDumpSymbolTableVisitor extends DumpSymbolTableVisitor {
 			case Struct.Class:
 				output.append("Class");
 				break;
+			case Struct.Interface:
+				output.append("Interface");
+				break;
 			}
 			break;
 		case Struct.Enum:
@@ -74,50 +77,4 @@ public class ExtendedDumpSymbolTableVisitor extends DumpSymbolTableVisitor {
 			break;
 		}
 	}
-	
-	// TODO: Update to include interfaces
-	/*
-	@Override
-	public void visitObjNode(Obj objToVisit) {
-		//output.append("[");
-		switch (objToVisit.getKind()) {
-		case Obj.Con:  output.append("Con "); break;
-		case Obj.Var:  output.append("Var "); break;
-		case Obj.Type: output.append("Type "); break;
-		case Obj.Meth: output.append("Meth "); break;
-		case Obj.Fld:  output.append("Fld "); break;
-		case Obj.Prog: output.append("Prog "); break;
-		}
-		
-		output.append(objToVisit.getName());
-		output.append(": ");
-		
-		if ((Obj.Var == objToVisit.getKind()) && "this".equalsIgnoreCase(objToVisit.getName()))
-			output.append("");
-		else
-			objToVisit.getType().accept(this);
-		
-		output.append(", ");
-		output.append(objToVisit.getAdr());
-		output.append(", ");
-		output.append(objToVisit.getLevel() + " ");
-				
-		if (objToVisit.getKind() == Obj.Prog || objToVisit.getKind() == Obj.Meth) {
-			output.append("\n");
-			nextIndentationLevel();
-		}
-		
-
-		for (Obj o : objToVisit.getLocalSymbols()) {
-			output.append(currentIndent.toString());
-			o.accept(this);
-			output.append("\n");
-		}
-		
-		if (objToVisit.getKind() == Obj.Prog || objToVisit.getKind() == Obj.Meth) 
-			previousIndentationLevel();
-
-		//output.append("]");
-		
-	}*/
 }

ac114d5659e4ecc3d1ef0b7c2300984733e32905
garidos <113151588+garidos@users.noreply.github.com>
2025-03-23 01:57:22 +0100

    Prevent stack overflow in symbol table dump visitor caused by cyclic type references

--- src/rs/ac/bg/etf/pp1/ExtendedDumpSymbolTableVisitor.java
+++ src/rs/ac/bg/etf/pp1/ExtendedDumpSymbolTableVisitor.java
@@ -3,6 +3,7 @@ package rs.ac.bg.etf.pp1;
 import java.io.ObjectOutput;
 
 import rs.etf.pp1.symboltable.concepts.Obj;
+import rs.etf.pp1.symboltable.concepts.Scope;
 import rs.etf.pp1.symboltable.concepts.Struct;
 import rs.etf.pp1.symboltable.visitors.DumpSymbolTableVisitor;
 
@@ -48,7 +49,8 @@ public class ExtendedDumpSymbolTableVisitor extends DumpSymbolTableVisitor {
 			output.append("Set");
 			break;
 		case Struct.Class:
-			output.append("Class ").append(structToVisit.getNumberOfFields()).append(" [");
+			output.append("Class ").append(" [");
+			// Methods are printed with a new line at the end, so no additional new line is necessary
 			boolean isPrevMethod = false;
 			currentIndent.append(indent);
 			for (Obj obj : structToVisit.getMembers()) {
@@ -56,7 +58,6 @@ public class ExtendedDumpSymbolTableVisitor extends DumpSymbolTableVisitor {
 				output.append(currentIndent.toString());
 				obj.accept(this);
 				if (obj.getKind() == Obj.Meth) isPrevMethod = true;
-				else isPrevMethod = false;
 			}
 			output.append("]");
 			previousIndentationLevel();
@@ -64,17 +65,65 @@ public class ExtendedDumpSymbolTableVisitor extends DumpSymbolTableVisitor {
 		case Struct.Interface:
 			output.append("Interface ").append(structToVisit.getNumberOfFields()).append(" [");
 			currentIndent.append(indent);
+			// Methods are printed with a new line at the end, so no additional new line is necessary
 			isPrevMethod = false;
 			for (Obj obj : structToVisit.getMembers()) {
 				if (!isPrevMethod) output.append('\n');
 				output.append(currentIndent.toString());
 				obj.accept(this);
 				if (obj.getKind() == Obj.Meth) isPrevMethod = true;
-				else isPrevMethod = false;
 			}
 			output.append("]");
 			previousIndentationLevel();
 			break;
 		}
+	}
+	
+	@Override
+	public void visitObjNode(Obj objToVisit) {
+		//output.append("[");
+		switch (objToVisit.getKind()) {
+		case Obj.Con:  output.append("Con "); break;
+		case Obj.Var:  output.append("Var "); break;
+		case Obj.Type: output.append("Type "); break;
+		case Obj.Meth: output.append("Meth "); break;
+		case Obj.Fld:  output.append("Fld "); break;
+		case Obj.Prog: output.append("Prog "); break;
+		}
+		
+		output.append(objToVisit.getName());
+		output.append(": ");
+		
+		if ((Obj.Var == objToVisit.getKind()) && "this".equalsIgnoreCase(objToVisit.getName()))
+			output.append("");
+		else if (objToVisit.getKind() == Obj.Fld && objToVisit.getType().getKind() == Struct.Class)
+			output.append("Class");
+		else if (objToVisit.getKind() == Obj.Fld && objToVisit.getType().getKind() == Struct.Interface)
+			output.append("Interface");
+		else
+			objToVisit.getType().accept(this);
+		
+		output.append(", ");
+		output.append(objToVisit.getAdr());
+		output.append(", ");
+		output.append(objToVisit.getLevel() + " ");
+				
+		if (objToVisit.getKind() == Obj.Prog || objToVisit.getKind() == Obj.Meth) {
+			output.append("\n");
+			nextIndentationLevel();
+		}
+		
+
+		for (Obj o : objToVisit.getLocalSymbols()) {
+			output.append(currentIndent.toString());
+			o.accept(this);
+			output.append("\n");
+		}
+		
+		if (objToVisit.getKind() == Obj.Prog || objToVisit.getKind() == Obj.Meth) 
+			previousIndentationLevel();
+
+		//output.append("]");
+		
 	}
 }

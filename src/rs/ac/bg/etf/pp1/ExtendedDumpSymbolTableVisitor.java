0ba72e7772b407d469187575e69b94a2741b82fe
garidos <113151588+garidos@users.noreply.github.com>
2025-03-26 02:33:16 +0100

    Update symbol table dump visitor
    - Only display complex type member information for Obj nodes of Type kind
    - Refactor

--- src/rs/ac/bg/etf/pp1/ExtendedDumpSymbolTableVisitor.java
+++ src/rs/ac/bg/etf/pp1/ExtendedDumpSymbolTableVisitor.java
@@ -1,9 +1,6 @@
 package rs.ac.bg.etf.pp1;
 
-import java.io.ObjectOutput;
-
 import rs.etf.pp1.symboltable.concepts.Obj;
-import rs.etf.pp1.symboltable.concepts.Scope;
 import rs.etf.pp1.symboltable.concepts.Struct;
 import rs.etf.pp1.symboltable.visitors.DumpSymbolTableVisitor;
 
@@ -12,22 +9,6 @@ public class ExtendedDumpSymbolTableVisitor extends DumpSymbolTableVisitor {
 	@Override
 	public void visitStructNode(Struct structToVisit) {
 		switch (structToVisit.getKind()) {
-		case Struct.None:
-			output.append("notype");
-			break;
-		case Struct.Int:
-			output.append("int");
-			break;
-		case Struct.Char:
-			output.append("char");
-			break;
-		case Struct.Bool:
-			output.append("bool");
-			break;
-		case Struct.Array:
-			output.append("Arr of ");
-			
-			switch (structToVisit.getElemType().getKind()) {
 			case Struct.None:
 				output.append("notype");
 				break;
@@ -37,76 +18,77 @@ public class ExtendedDumpSymbolTableVisitor extends DumpSymbolTableVisitor {
 			case Struct.Char:
 				output.append("char");
 				break;
+			case Struct.Bool:
+				output.append("bool");
+				break;
+			case Struct.Array:
+				output.append("Arr of ");
+				
+				switch (structToVisit.getElemType().getKind()) {
+				case Struct.None:
+					output.append("notype");
+					break;
+				case Struct.Int:
+					output.append("int");
+					break;
+				case Struct.Char:
+					output.append("char");
+					break;
+				case Struct.Class:
+					output.append("Class");
+					break;
+				case Struct.Interface:
+					output.append("Interface");
+					break;
+				}
+				break;
+			case Struct.Enum:
+				output.append("Set");
+				break;
 			case Struct.Class:
 				output.append("Class");
+				printMembers(structToVisit);
 				break;
 			case Struct.Interface:
-				output.append("Interface");
+				output.append("Interface ");
+				printMembers(structToVisit);
 				break;
-			}
-			break;
-		case Struct.Enum:
-			output.append("Set");
-			break;
-		case Struct.Class:
-			output.append("Class ").append(" [");
-			// Methods are printed with a new line at the end, so no additional new line is necessary
-			boolean isPrevMethod = false;
-			currentIndent.append(indent);
-			for (Obj obj : structToVisit.getMembers()) {
-				if (!isPrevMethod) output.append('\n');
-				output.append(currentIndent.toString());
-				obj.accept(this);
-				if (obj.getKind() == Obj.Meth) isPrevMethod = true;
-			}
-			output.append("]");
-			previousIndentationLevel();
-			break;
-		case Struct.Interface:
-			output.append("Interface ").append(structToVisit.getNumberOfFields()).append(" [");
-			currentIndent.append(indent);
-			// Methods are printed with a new line at the end, so no additional new line is necessary
-			isPrevMethod = false;
-			for (Obj obj : structToVisit.getMembers()) {
-				if (!isPrevMethod) output.append('\n');
-				output.append(currentIndent.toString());
-				obj.accept(this);
-				if (obj.getKind() == Obj.Meth) isPrevMethod = true;
-			}
-			output.append("]");
-			previousIndentationLevel();
-			break;
 		}
 	}
 	
 	@Override
 	public void visitObjNode(Obj objToVisit) {
-		//output.append("[");
 		switch (objToVisit.getKind()) {
-		case Obj.Con:  output.append("Con "); break;
-		case Obj.Var:  output.append("Var "); break;
-		case Obj.Type: output.append("Type "); break;
-		case Obj.Meth: output.append("Meth "); break;
-		case Obj.Fld:  output.append("Fld "); break;
-		case Obj.Prog: output.append("Prog "); break;
+			case Obj.Con:  output.append("Con "); break;
+			case Obj.Var:  output.append("Var "); break;
+			case Obj.Type: output.append("Type "); break;
+			case Obj.Meth: output.append("Meth "); break;
+			case Obj.Fld:  output.append("Fld "); break;
+			case Obj.Prog: output.append("Prog "); break;
 		}
 		
-		output.append(objToVisit.getName());
-		output.append(": ");
+		output.append(objToVisit.getName())
+			.append(": ");
 		
-		if ((Obj.Var == objToVisit.getKind()) && "this".equalsIgnoreCase(objToVisit.getName()))
+		if ((Obj.Var == objToVisit.getKind()) && "this".equalsIgnoreCase(objToVisit.getName())) {
 			output.append("");
-		else if (objToVisit.getKind() == Obj.Fld && objToVisit.getType().getKind() == Struct.Class)
+		}
+		else if (objToVisit.getKind() != Obj.Type && objToVisit.getType().getKind() == Struct.Class) {
+			// Member information will only be printed out for type definitions
 			output.append("Class");
-		else if (objToVisit.getKind() == Obj.Fld && objToVisit.getType().getKind() == Struct.Interface)
+		}
+		else if (objToVisit.getKind() != Obj.Type && objToVisit.getType().getKind() == Struct.Interface) {
+			// Member information will only be printed out for type definitions
 			output.append("Interface");
-		else
+		}
+		else {
 			objToVisit.getType().accept(this);
+		}	
 		
-		output.append(", ");
-		output.append(objToVisit.getAdr());
-		output.append(", ");
-		output.append(objToVisit.getLevel() + " ");
+		output.append(", ")
+			.append(objToVisit.getAdr())
+			.append(", ")
+			.append(objToVisit.getLevel() + " ");
 				
 		if (objToVisit.getKind() == Obj.Prog || objToVisit.getKind() == Obj.Meth) {
 			output.append("\n");
@@ -120,10 +102,29 @@ public class ExtendedDumpSymbolTableVisitor extends DumpSymbolTableVisitor {
 			output.append("\n");
 		}
 		
-		if (objToVisit.getKind() == Obj.Prog || objToVisit.getKind() == Obj.Meth) 
+		if (objToVisit.getKind() == Obj.Prog || objToVisit.getKind() == Obj.Meth) {
 			previousIndentationLevel();
-
-		//output.append("]");
+		}		
+	}
+	
+	private void printMembers(Struct complexTypeStruct) {
+		output.append(" [");
+		currentIndent.append(indent);
 		
+		// Methods are printed with a new line at the end, so no additional new line is necessary
+		var isPrevMethod = false;
+		
+		for (var obj : complexTypeStruct.getMembers()) {
+			if (!isPrevMethod) output.append('\n');
+			
+			output.append(currentIndent.toString());
+			obj.accept(this);
+			
+			if (obj.getKind() == Obj.Meth) isPrevMethod = true;
+			else isPrevMethod = false;
+		}
+		
+		output.append("]");
+		previousIndentationLevel();
 	}
 }
